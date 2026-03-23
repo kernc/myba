450cef2d7ec43be38ab89eb6f6984c85c2af27e9
Kernc <kerncece@gmail.com>
2026-03-23 20:25:57 +0100

    BUG: Fix 'myba pw' echo bugs. **Invalidates all current passwords** input by `myba pw`

--- myba.sh
+++ myba.sh
@@ -122,15 +122,13 @@ _read_vars () {
 cmd_pw () {
     stty -echo
     {
-        IFS= read -p "Enter encryption PASSWORD=: " -r PASSWORD
-        echo
+        IFS= read -p "Enter encryption PASSWORD=: " -r PASSWORD && echo >&2
         (
-            IFS= read -p "Repeat: " -r PASSWORD2
+            IFS= read -p "Repeat: " -r PASSWORD2 && echo >&2
             [ "$PASSWORD" = "$PASSWORD2" ] || { warn 'ERROR: Password mismatch!'; exit 1; }
         )
     } < /dev/tty
     stty echo
-    echo
     echo "$PASSWORD"
 }
 _ask_pw () {
